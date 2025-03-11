6d1646014e7b01f2613f465664aa220cd8288d58
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-03-11 15:36:27 -0400

    Chapter_1: Character Counting

--- Chapter_1/hello.c
+++ Chapter_1/hello.c
@@ -3,6 +3,18 @@
 
 int main(){
     printf("Hello, World!\n");
+    
+    int c;
+    
+    printf("%d", EOF);
+    
+    c = getchar() != EOF;
+
+    printf("%d", c);
+
+
+    
+    
 
     return EXIT_SUCCESS;
 }
