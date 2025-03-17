1db013fc4900cdc5869af5b695a08bfe97f23be1
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-03-17 11:29:16 +0000

    Modern_C_Level_1/Structures.c

--- Modern_C_Level_1/Structures.c
+++ Modern_C_Level_1/Structures.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct BirdStruct
+{
+    char const* jay;
+    char const* magpie;
+    char const* raven;
+    char const* chough;
+};
+
 int main(){
 
+    struct BirdStruct const aName = {
+        .jay = "Joe",
+        .magpie = "Frau",
+        .raven = "Lissy",
+        .chough = "Henry"
+    };
+
+    printf("List Of Birds Name:\n");
+    printf("%s\n%s\n%s\n%s\n", aName.chough, aName.jay, aName.magpie, aName.raven);
+
+    /* TO DO */
+    // Loop through the array and display the name preceded with an index.
+    //int elementCount;
     
     return EXIT_SUCCESS;
 }
