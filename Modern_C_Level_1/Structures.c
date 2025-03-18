758c4d6c60015fba32ea1b5a747bbbd92e891748
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-03-18 10:47:09 -0400

    Modern_C_Level_1/Structures.c

--- Modern_C_Level_1/Structures.c
+++ Modern_C_Level_1/Structures.c
@@ -21,9 +21,27 @@ int main(){
     printf("List Of Birds Name:\n");
     printf("%s\n%s\n%s\n%s\n", aName.chough, aName.jay, aName.magpie, aName.raven);
 
-    /* TO DO */
-    // Loop through the array and display the name preceded with an index.
-    //int elementCount;
+    /* TO DO 
+    Loop through the struct object and display the name preceded with an index.
+     e.g. 1. Joe
+          2. Frau
+          3. Lissy
+          4. Henry
+    */
+
+    // Create an array to store the names of the birds
+    char const* birdNames[] = {aName.jay, aName.magpie, aName.raven, aName.chough};
+
+    // Calculate the number of elements in the array
+    int arrayElements = sizeof(birdNames) / sizeof(birdNames[0]);
+    
+    printf("\nList Of Birds Name:\n");
+    
+    // Now loop through the array and display the name preceded with an index
+    for (int i = 0; i < arrayElements; i++)
+    {
+        printf("%d. %s\n", i + 1, birdNames[i]);
+    }
     
     return EXIT_SUCCESS;
 }
