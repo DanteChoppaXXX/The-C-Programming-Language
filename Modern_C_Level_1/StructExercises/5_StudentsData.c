73efec829ec58b6ac7fe8b9d8297972e00794dc6
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-03-18 21:10:20 -0400

    5_StudentsData.c

--- Modern_C_Level_1/StructExercises/5_StudentsData.c
+++ Modern_C_Level_1/StructExercises/5_StudentsData.c
@@ -14,11 +14,27 @@ typedef struct
 
 int main(){
 
-    
+    Student students[5][4] = {
+        {"John Jones", 21, 9, 89.5},
+        {"Mary Jackson", 19, 6, 79.6},
+        {"Hunter Banes", 23, 9, 85.7},
+        {"Dante Choppa", 25, 419, 99.9},
+        {"Kyle Xavi", 30, 5, 90.8},
+    };
 
     printf("\nSTUDENT DETAILS:\n");
     printf("================\n");
 
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Student %d\n", i + 1);
+        printf("Name: %s\n", students[i][0].name);
+        printf("Age: %d\n", students[i][0].age);
+        printf("Roll Number: %d\n", students[i][0].rollNumber);
+        printf("Mark: %.1f\n", students[i][0].mark);
+        printf("\n");
+    }
+    
 
     return EXIT_SUCCESS;
 }
