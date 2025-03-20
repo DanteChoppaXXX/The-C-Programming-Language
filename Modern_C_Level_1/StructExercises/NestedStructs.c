dfa7d33ad18e1093dc2cfe4589daa75a4ca6a868
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-03-20 11:33:21 +0000

    NestedStructs.c

--- Modern_C_Level_1/StructExercises/NestedStructs.c
+++ Modern_C_Level_1/StructExercises/NestedStructs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Define a struct Date(day, month, year).
 typedef struct
@@ -15,17 +16,41 @@ typedef struct
 {
     char name[50];
     int id;
-    float salary;
-    
+    double salary;
+    Date joiningDate;
 
 }Employee;
 
 
 int main()
 {
+    // Create an employee object of type (struct Employee).
+    Employee employee;
 
+    /* Take input and display an employee’s data. */
 
-    // Take input and display an employee’s details.
+    // Prompt the employee for their data.
+    printf("Enter your full name: \n");
+    fgets(employee.name, 50, stdin);
+    employee.name[strlen(employee.name) - 1] = '\0';
+
+    printf("Enter your ID number: \n");
+    scanf("%d", &employee.id);
+
+    printf("Enter your exact salary amount(e.g 50000.55): \n");
+    scanf("%lf", &employee.salary);
+
+    printf("Enter the date you joined the company(e.g dd mm yyyy): \n");
+    scanf("%d %d %d", &employee.joiningDate.day, &employee.joiningDate.month, &employee.joiningDate.year);
+
+    // Display the employee's data.
+    printf("\nEMPLOYEE'S DATA:\n");
+    printf("==================\n");
+
+    printf("Name: %s\n",employee.name);
+    printf("ID: %d\n",  employee.id);
+    printf("Salary: $%.2lf\n", employee.salary);
+    printf("Join Date: %d/%d/%d", employee.joiningDate.day, employee.joiningDate.month, employee.joiningDate.year);
 
 
     return EXIT_SUCCESS;
