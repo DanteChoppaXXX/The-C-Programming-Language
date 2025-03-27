822c8ff331443f3a7601aa4642067f90784ecfb7
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-03-27 01:58:24 +0000

    ReverseArray.c

--- Modern_C_Level_1/PointersExercises/ReverseArray.c
+++ Modern_C_Level_1/PointersExercises/ReverseArray.c
@@ -7,32 +7,45 @@ Reverse an array in-place using pointers without using extra space.
 */
 
 // Function Prototype.
-void reverseArray(int* array);
+void reverseArray(int* array, int size);
 
 
 int main()
 {
-   int numbers[] = {1, 2, 3, 4, 5};
+    int numbers[] = {25, 20, 15, 10, 5};
 
-   int size = sizeof(numbers);
+    int size = sizeof(numbers) / sizeof(numbers[0]);
 
+    int* pNumbers; 
+    pNumbers = numbers;
 
     // Display elements of the array before reversing.
     printf("\nBEFORE REVERSING:\n");
     printf("=================\n");
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\n", pNumbers[i]);
+    }
     
-    
-    reverseArray();
     
     // Display elements of the array after reversing.
     printf("\nAFTER REVERSING:\n");
     printf("================\n");
     
+    reverseArray(pNumbers, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\n", pNumbers[i]);
+    }
+    
+    
     return EXIT_SUCCESS;
 }
 
 // Function to reverse array.
-void reverseArray(int* array)
+void reverseArray(int* array, int size)
 {
     
 }
