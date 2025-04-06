120d76c9dc8ac193b0d0b90e3f8eb0b4e4d52591
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-04-05 20:10:59 -0400

    Advanced.c

--- UNDERSTAND/ArrayIndexingAndMemoryLayout/Advanced.c
+++ UNDERSTAND/ArrayIndexingAndMemoryLayout/Advanced.c
@@ -16,7 +16,95 @@ Concepts Covered: Dynamic 2D arrays, pointer-to-pointer (int**), manual memory m
 
 int main()
 {
+    int rows, cols;
     
+    // Prompt the user for number of rows and columns.
+    printf("Enter the number of rows: \n");
+    scanf("%d", &rows);
     
+    printf("Enter the number of columns: \n");
+    scanf("%d", &cols);
+
+    // Allocate memory for the 2D array using an array of pointers (int**).
+    int** pArray = malloc(rows * sizeof(int*));
+    printf("\nArray Memory Address: %p\n", pArray);
+
+    // Allocate memory for each row (i.e each pointer) in the array.
+    for (int i = 0; i < rows; i++)
+    {
+        pArray[i] = malloc(cols * sizeof(int));
+    }
+    
+    // Fill the array with values such that arr[i][j] = i * j.
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            pArray[i][j] = i * j;
+        }
+        // printf("\n");
+    }
+    
+    printf("\n");
+
+    // Display the array using array indexing.
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", pArray[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n");
+
+    // Display the array using pointer arithmetic.
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", *(*(pArray + i) + j));
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    // Display the address of the elements in the matrix using array notation.
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%p ", &pArray[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    // Display the address of the elements in the matrix using pointer arthimetic.
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%p ", *(pArray + i) + j);
+        }
+        printf("\n");
+    }
+    
+    printf("\n");
+    
+    // Free the allocated memory of each row in the array.
+    for (int i = 0; i < rows; i++)
+    {
+        free(pArray[i]);
+        printf("Memory Address: %p Successfully Freed!\n", pArray[i]);
+    }
+    
+    // Free the allocated memory of the array.
+    free(pArray);
+    printf("\nMemory Address: %p Successfully Freed!\n", pArray);
+
     return EXIT_SUCCESS;
 }
