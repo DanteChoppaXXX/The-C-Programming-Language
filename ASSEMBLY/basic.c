bf0429dba2ddf1efeb6f29b72048879f3328b600
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-04-07 08:47:42 -0400

    ASSEMBLY

--- ASSEMBLY/basic.c
+++ ASSEMBLY/basic.c
@@ -4,8 +4,17 @@
 // Function Prototype.
 int add(int a, int b);
 
-int main(){
+int main()
+{
+    int a = 5;
+    int b = 6;
 
+    add(a, b);
    
     return EXIT_SUCCESS;
+}
+
+int add(int a, int b)
+{
+    return (a + b);
 }
---
+++ ASSEMBLY/basic.s
@@ -0,0 +1,50 @@
+	.file	"basic.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$5, -4(%rbp)
+	movl	$6, -8(%rbp)
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	add
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.globl	add
+	.type	add, @function
+add:
+.LFB7:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	add, .-add
+	.ident	"GCC: (Debian 14.2.0-16) 14.2.0"
+	.section	.note.GNU-stack,"",@progbits
