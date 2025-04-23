c298cc5d51447b7e9f66da6d2dd365b7515e0279
DanteChoppaXXX <only1dantechoppa@gmail.com>
2025-04-23 07:04:48 -0400

    LimboShell Phase 1

--- PROJECTS/LIMBOSHELL/src/limboShell.c
+++ PROJECTS/LIMBOSHELL/src/limboShell.c
@@ -7,10 +7,45 @@
 #include <sys/errno.h>
 #include <signal.h>
 
+#define BUFFER_SIZE 1024
+
 
 int main()
 {
+    printf("Limbo $hell...\n");
+    // Create command input buffer.
+    char command[BUFFER_SIZE];
+    
+    // Display a prompt.
+    printf("[#]> ");
+    fflush(stdout);
 
+    // Start an infinite loop to keep the shell running.
+    while ((atoi(fgets(command, BUFFER_SIZE, stdin))) != EOF)
+    {
+        
+
+        
+        
+        // Read input.
+        // fgets(command, BUFFER_SIZE, stdin);
+        command[strcspn(command, "\n")] = '\0';
+
+        printf("%s\n", command);
+
+        printf("[#]> ");
+        fflush(stdout);
+
+        // Parse the command.
+        /* code */
+
+        // Execute the command.
+        /* code */
+        
+        // Repeat.
+    } 
+
+    printf("Exiting...\n");
     
     return EXIT_SUCCESS;
 }
